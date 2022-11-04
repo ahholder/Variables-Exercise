afea3e0e618bc999e98da538084d9e6fd9f0820e
Alexander Holder <drowish95z@gmail.com>
2022-11-04 09:52:30 -0500

    Enabled test output for console writeline... after re-reading the instructions and realizing that was, in fact, a thing.

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -12,14 +12,14 @@
             double registerBalance = 100.00;
             decimal carriedCash = 10.00m;
             //Test
-            /*Console.WriteLine($"{customerName} ({middleInitial}.) is {customerAge} years old and carrying ${carriedCash}. Our register's balance is {registerBalance}.");
+            Console.WriteLine($"{customerName} ({middleInitial}.) is {customerAge} years old and carrying ${carriedCash}. Our register's balance is {registerBalance}.");
             if (cardMember == true)
             {
                 Console.WriteLine($"{customerName} is a rewards member.");
             } else
             {
                 Console.WriteLine($"{customerName} is NOT a rewards member.");
-            }*/
+            }
         }
     }
 }
