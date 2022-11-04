bdcaa7c8739616a519a3454be2ddb6e232403fc0
Alexander Holder <drowish95z@gmail.com>
2022-11-04 09:50:28 -0500

    Added Variables and tested their viability

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            string customerName = "Alexander the Great";
+            int customerAge = 27;
+            char middleInitial = 'T';
+            bool cardMember = true;
+            double registerBalance = 100.00;
+            decimal carriedCash = 10.00m;
+            //Test
+            /*Console.WriteLine($"{customerName} ({middleInitial}.) is {customerAge} years old and carrying ${carriedCash}. Our register's balance is {registerBalance}.");
+            if (cardMember == true)
+            {
+                Console.WriteLine($"{customerName} is a rewards member.");
+            } else
+            {
+                Console.WriteLine($"{customerName} is NOT a rewards member.");
+            }*/
         }
     }
 }
